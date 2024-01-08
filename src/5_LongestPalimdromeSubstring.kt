a4f28429576aa09d2cc0cd3a0bfe1d2ec470b059
vikramsingh <mr.vikram004@gmail.com>
2024-01-09 02:49:56 +0400

    5 longest palimdrome substring, with recursion, topDown approach added

--- src/5_LongestPalimdromeSubstring.kt
+++ src/5_LongestPalimdromeSubstring.kt
@@ -2,13 +2,48 @@ import kotlin.math.max
 
 fun main() {
    // val result1 = longestPalindrome("babad")
-    val result = longestPalindrome("cbbd")
+    val result = longestPalindrome("a")
     println(result)
 }
 
+fun longestPalindrome(s: String): String {
+    val subArray = s.toCharArray()
+
+    var sub = ""
+    var maxLength = Integer.MIN_VALUE
+
+    val dp = Array(subArray.size) { IntArray(subArray.size) {-1} }
+
+    for (diff in 0..subArray.lastIndex) {
+        var i =0
+        var j = i+diff
+        while (j <= subArray.lastIndex) {
+            if(i == j) {
+                dp[i][j] = 1
+            } else if(1 == diff) {
+                dp[i][j] = if(subArray[i]==subArray[j])  2 else  0
+            } else if((subArray[i]==subArray[j]) && (0 < dp[i+1][j-1])) {
+                dp[i][j] = dp[i+1][j-1] + 2
+            }
+
+            if(0 < dp[i][j]) {
+                if(j-i +1 > maxLength) {
+                    maxLength = j-i +1
+                    sub = s.substring(i, maxLength+i)
+                }
+            }
+            i++
+            j++
+        }
+    }
+
+    return sub
+}
+
+
 val array = Array(1000) { IntArray(1000) {-1} }
 
-fun longestPalindrome(s: String): String {
+fun longestPalindrome1(s: String): String {
     val subArray = s.toCharArray()
 
     var starPosition = 0
